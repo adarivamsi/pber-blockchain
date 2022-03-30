ed29c96eb283fc9505f6eda46b6a8ee367b7fe56
Vamsi Charan Adari <adarivamsi@gmail.com>
2022-03-30 15:12:43 -0500

    Create a blockchain authentication Uber clone app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { PberProvider } from "../context/pberContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PberProvider>
+      <Component {...pageProps} />
+    </PberProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
